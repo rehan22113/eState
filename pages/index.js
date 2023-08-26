cef54cada18823d38e010108df685dba789b974f
rehan22113 <raorehanweb@gmail.com>
2023-08-26 19:43:57 +0500

    new 1

--- pages/index.js
+++ pages/index.js
@@ -209,7 +209,7 @@ const Home = () => {
     <div className="flex flex-wrap -m-4">
       <div className="p-4 lg:w-1/4 md:w-1/2">
         <div className="h-full flex flex-col items-center text-center">
-          <img alt="team" className="flex-shrink-0 w-64 h-60 mb-4" src="/2.jpg" />
+          <img alt="team" className="flex-shrink-0 w-52 h-52 mb-4" src="/2.jpg" />
           <div className="w-full">
             <h2 className="title-font font-medium text-lg text-gray-900">Parv Prabhakar</h2>
             <h3 className="text-gray-500 mb-3">CEO</h3>
@@ -228,7 +228,7 @@ const Home = () => {
       </div>
       <div className="p-4 lg:w-1/4 md:w-1/2">
         <div className="h-full flex flex-col items-center text-center">
-          <img alt="team" className="flex-shrink-0 w-64 h-60 mb-4" src="/1.jpeg" />
+          <img alt="team" className="flex-shrink-0 w-52 h-52 mb-4" src="/1.jpeg" />
           <div className="w-full">
             <h2 className="title-font font-medium text-lg text-gray-900">Ryan smith</h2>
             <h3 className="text-gray-500 mb-3">Head of Real Estate</h3>
@@ -244,7 +244,7 @@ const Home = () => {
       </div>
       <div className="p-4 lg:w-1/4 md:w-1/2">
         <div className="h-full flex flex-col items-center text-center">
-          <img alt="team" className="flex-shrink-0 w-64 h-60 mb-4" src="/rectangle-11@2x.png" />
+          <img alt="team" className="flex-shrink-0 w-52 h-52 mb-4" src="/rectangle-11@2x.png" />
           <div className="w-full">
             <h2 className="title-font font-medium text-lg text-gray-900">Cvic Innocent</h2>
             <h3 className="text-gray-500 mb-3">CTO</h3>
@@ -259,7 +259,7 @@ const Home = () => {
       </div>
       <div className="p-4 lg:w-1/4 md:w-1/2">
         <div className="h-full flex flex-col items-center text-center">
-          <img alt="team" className="flex-shrink-0 w-64 h-60 mb-4" src="/rec12.jpeg" />
+          <img alt="team" className="flex-shrink-0 w-52 h-52 mb-4" src="/rec12.jpeg" />
           <div className="w-full">
             <h2 className="title-font font-medium text-lg text-gray-900">Abhinav Kumar</h2>
             <h3 className="text-gray-500 mb-3 capitalize">Head of Marketing</h3>
